4153584d3b78e24602161997cb2fa9d612eac911
Bryan Konowitz <bryan@kono.sh>
2015-09-19 23:06:43 -0700

    updating for dokku compat

--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :shrty, Shrty.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "young-hamlet-8505.herokuapp.com", port: 443],
+  url: [scheme: "https", host: System.get_env("HOSTNAME"), port: 80],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json"
 
