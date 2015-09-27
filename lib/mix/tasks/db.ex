08d6859921343c7fddbf6c39f27e224f760fff8b
Bryan Konowitz <bryan@kono.sh>
2015-09-26 23:47:21 -0700

    missed the mnesia_dir settings in the mix tasks. woops.

--- lib/mix/tasks/db.ex
+++ lib/mix/tasks/db.ex
@@ -13,6 +13,7 @@ defmodule Mix.Tasks.Shrty do
         # starting mnesia itself, the schema gets stored on disk based on the
         # `-mnesia` config, so you don't really need to create it every time.
         IO.puts "loading up Amnesia"
+        :application.set_env(:mnesia, :dir, Shrty.mnesia_dir)
         Amnesia.Schema.create
 
         # Once the schema has been created, you can start mnesia.
@@ -52,6 +53,7 @@ defmodule Mix.Tasks.Shrty do
       """
       def run(_) do
         # Start mnesia, or we can't do much.
+        :application.set_env(:mnesia, :dir, Shrty.mnesia_dir)
         Amnesia.start
 
         # Destroy the database.
