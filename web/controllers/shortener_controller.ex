3bd68f9b689b3f9ebbd2c6420cb99b3d92d2e739
Bryan Konowitz <bryan@kono.sh>
2016-02-09 20:09:54 -0800

    better logging of redirects

--- web/controllers/shortener_controller.ex
+++ web/controllers/shortener_controller.ex
@@ -1,5 +1,6 @@
 defmodule Shrty.ShortenerController do
   use Shrty.Web, :controller
+  require Logger
 
   def create(conn, %{"url" => url}) do
     token = Shrty.Shortener.shrink(url)
@@ -7,14 +8,17 @@ defmodule Shrty.ShortenerController do
   end
 
   def show(conn, %{"token" => token}) do
-    redirect_to_url(conn, Shrty.Shortener.expand(token))
+    redirect_to_url(conn, token, Shrty.Shortener.expand(token))
   end
 
-  defp redirect_to_url(conn, nil) do
+  defp redirect_to_url(conn, _token, nil) do
     conn
     |> put_status(:not_found)
     |> render(Shrty.ErrorView, "404.html")
   end
 
-  defp redirect_to_url(conn, url), do: redirect(conn, external: url)
+  defp redirect_to_url(conn, token, url) do
+    Logger.info "Valid token [ #{token} ], redirecting to: [ #{url} ]"
+    redirect(conn, external: url)
+  end
 end
